7eac6c3d6d1b5f902c18025f8c43e943244506c0
abhisaxena5694 <abhisaxena5694@gmail.com>
2016-03-18 03:41:12 +0530

    Create quotations.py

---
+++ quotations.py
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> Inspiring Quotations</title>
+  <head>
+  <body>
+    <p>These are some quotations by few of the most influential people-</p>
+    <ol>
+      <li>Michael Jordan-</li>
+      <blockquote>I’ve missed more than 9000 shots in my career.
+      I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. 
+      I’ve failed over and over and over again in my life. And that is why I succeed.
+      </blockquote>
+      </li>
+    </ol>
+  </body>
+</html>
